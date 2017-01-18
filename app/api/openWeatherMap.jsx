dda978b53461c02dca87d044fe3e7da4ceb36bec
Rhayne Moore <rhayne.moore@neovation.local>
2017-01-18 15:09:15 -0600

    initial commit

---
+++ app/api/openWeatherMap.jsx
@@ -0,0 +1,21 @@
+var axios = require('axios');
+
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=718623e2d1d91f418ae7ecc06d455700&units=metric';
+
+module.exports = {
+  getTemp: function (location) {
+    var encodedLocation = encodeURIComponent(location);
+    var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
+
+    return axios.get(requestUrl).then(function (res) {
+      debugger;
+      if (res.data.cod && res.data.message) {
+        throw new Error(res.data.message);
+      } else {
+        return res.data.main.temp;
+      }
+    }, function (res) {
+      throw new Error(res.data.message);
+    });
+  }
+}
